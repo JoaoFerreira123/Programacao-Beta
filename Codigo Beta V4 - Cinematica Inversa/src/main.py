3e2e22b9803a2755c81d2a4f59b5fe58a1d6c436
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2023-02-26 23:04:21 -0300

    ok

--- Codigo Beta V4 - Cinematica Inversa/src/main.py
+++ Codigo Beta V4 - Cinematica Inversa/src/main.py
@@ -1,6 +1,14 @@
 from tkinter import *
 import serial
 
+#Bibliotecas para IK
+import ikpy.chain
+import ikpy.utils.plot as plot_utils
+import numpy as np
+import time
+import math
+import ipywidgets as widgets
+
 w = Tk()
 w.minsize(400, 200)
 w.title('Painel de Controle Beta')
